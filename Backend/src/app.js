69f3b77bb41e76146848d49ea573ca90a8349885
iambuzzz <ambujjais1@gmail.com>
2025-12-22 14:58:31 +0530

    DB Connected

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const dbConnect = require("./config/dbConnect");
+
 const app = express();
-app.get("/", (req, res) => {
-    res.send("Home!!");
-});
-app.get("/about", (req, res) => {
-    res.send("about!");
-});
-app.listen(3000, () => {
-    console.log("server is running on port 3000...");
-});
+
+const start = async () => {
+  try {
+    await dbConnect();
+    app.listen(3000, () => {
+      console.log("Server running on port 3000...");
+    });
+  } catch (err) {
+    console.error("Startup failed", err);
+  }
+};
+
+start();
