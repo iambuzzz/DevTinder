e2d90f3b83362901c10838be7da8d83b720bed63
iambuzzz <ambujjais1@gmail.com>
2025-12-26 16:58:57 +0530

    F: Basic setup done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Profile from "./components/Profile";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Hello</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,166 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      Login Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+      perspiciatis officiis delectus animi nemo repellendus molestiae sed esse
+      provident aliquid ducimus explicabo adipisci harum, illum unde. Quidem
+      consectetur suscipit libero! Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Deserunt ex, rerum fugiat laborum voluptatum quisquam
+      ducimus earum dignissimos nemo nulla eaque nisi provident ad molestiae
+      beatae quas blanditiis nostrum! Odit? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Excepturi iusto perferendis tempore
+      voluptatem quod corporis quidem, iste dolores odit nobis corrupti aut
+      rerum sit laborum nam labore sapiente, accusantium eligendi? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Laboriosam consectetur enim
+      cum ab, pariatur, tempora reiciendis alias expedita saepe, inventore aut
+      perspiciatis. Molestiae, tenetur aliquid corrupti facilis ipsa amet ipsum?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione provident
+      quasi culpa rem neque eos, saepe dolor facere numquam voluptate, esse
+      officiis quis cumque adipisci! Voluptatibus, neque? Eius est culpa quo
+      quos ipsam, recusandae voluptatibus praesentium et alias facilis dolores.
+      Eum dignissimos aut distinctio molestias architecto quisquam eius commodi
+      iusto ipsum, optio debitis rem corrupti similique quasi? Reprehenderit,
+      commodi? Quisquam velit earum blanditiis, est deleniti repudiandae quae
+      iste, aperiam similique expedita id optio enim obcaecati repellat
+      inventore veritatis quaerat nisi, tempora reprehenderit tempore dolorem!
+      Asperiores reprehenderit ducimus itaque corporis debitis quae! Delectus
+      vero sed magnam ut sit numquam reprehenderit voluptatibus. Login Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Eius perspiciatis
+      officiis delectus animi nemo repellendus molestiae sed esse provident
+      aliquid ducimus explicabo adipisci harum, illum unde. Quidem consectetur
+      suscipit libero! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Deserunt ex, rerum fugiat laborum voluptatum quisquam ducimus earum
+      dignissimos nemo nulla eaque nisi provident ad molestiae beatae quas
+      blanditiis nostrum! Odit? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Excepturi iusto perferendis tempore voluptatem quod
+      corporis quidem, iste dolores odit nobis corrupti aut rerum sit laborum
+      nam labore sapiente, accusantium eligendi? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Laboriosam consectetur enim cum ab,
+      pariatur, tempora reiciendis alias expedita saepe, inventore aut
+      perspiciatis. Molestiae, tenetur aliquid corrupti facilis ipsa amet ipsum?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione provident
+      quasi culpa rem neque eos, saepe dolor facere numquam voluptate, esse
+      officiis quis cumque adipisci! Voluptatibus, neque? Eius est culpa quo
+      quos ipsam, recusandae voluptatibus praesentium et alias facilis dolores.
+      Eum dignissimos aut distinctio molestias architecto quisquam eius commodi
+      iusto ipsum, optio debitis rem corrupti similique quasi? Reprehenderit,
+      commodi? Quisquam velit earum blanditiis, est deleniti repudiandae quae
+      iste, aperiam similique expedita id optio enim obcaecati repellat
+      inventore veritatis quaerat nisi, tempora reprehenderit tempore dolorem!
+      Asperiores reprehenderit ducimus itaque corporis debitis quae! Delectus
+      vero sed magnam ut sit numquam reprehenderit voluptatibus. Login Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Eius perspiciatis
+      officiis delectus animi nemo repellendus molestiae sed esse provident
+      aliquid ducimus explicabo adipisci harum, illum unde. Quidem consectetur
+      suscipit libero! Lorem ipsum dolor sit amet consectetur adipisicing
+      elit.Login Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+      perspiciatis officiis delectus animi nemo repellendus molestiae sed esse
+      provident aliquid ducimus explicabo adipisci harum, illum unde. Quidem
+      consectetur suscipit libero! Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Deserunt ex, rerum fugiat laborum voluptatum quisquam
+      ducimus earum dignissimos nemo nulla eaque nisi provident ad molestiae
+      beatae quas blanditiis nostrum! Odit? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Excepturi iusto perferendis tempore
+      voluptatem quod corporis quidem, iste dolores odit nobis corrupti aut
+      rerum sit laborum nam labore sapiente, accusantium eligendi? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Laboriosam consectetur enim
+      cum ab, pariatur, tempora reiciendis alias expedita saepe, inventore aut
+      perspiciatis. Molestiae, tenetur aliquid corrupti facilis ipsa amet ipsum?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione provident
+      quasi culpa rem neque eos, saepe dolor facere numquam voluptate, esse
+      officiis quis cumque adipisci! Voluptatibus, neque? Eius est culpa quo
+      quos ipsam, recusandae voluptatibus praesentium et alias facilis dolores.
+      Eum dignissimos aut distinctio molestias architecto quisquam eius commodi
+      iusto ipsum, optio debitis rem corrupti similique quasi? Reprehenderit,
+      commodi? Quisquam velit earum blanditiis, est deleniti repudiandae quae
+      iste, aperiam similique expedita id optio enim obcaecati repellat
+      inventore veritatis quaerat nisi, tempora reprehenderit tempore dolorem!
+      Asperiores reprehenderit ducimus itaque corporis debitis quae! Delectus
+      vero sed magnam ut sit numquam reprehenderit voluptatibus. Login Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Eius perspiciatis
+      officiis delectus animi nemo repellendus molestiae sed esse provident
+      aliquid ducimus explicabo adipisci harum, illum unde. Quidem consectetur
+      suscipit libero! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Deserunt ex, rerum fugiat laborum voluptatum quisquam ducimus earum
+      dignissimos nemo nulla eaque nisi provident ad molestiae beatae quas
+      blanditiis nostrum! Odit? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Excepturi iusto perferendis tempore voluptatem quod
+      corporis quidem, iste dolores odit nobis corrupti aut rerum sit laborum
+      nam labore sapiente, accusantium eligendi? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Laboriosam consectetur enim cum ab,
+      pariatur, tempora reiciendis alias expedita saepe, inventore aut
+      perspiciatis. Molestiae, tenetur aliquid corrupti facilis ipsa amet ipsum?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione provident
+      quasi culpa rem neque eos, saepe dolor facere numquam voluptate, esse
+      officiis quis cumque adipisci! Voluptatibus, neque? Eius est culpa quo
+      quos ipsam, recusandae voluptatibus praesentium et alias facilis dolores.
+      Eum dignissimos aut distinctio molestias architecto quisquam eius commodi
+      iusto ipsum, optio debitis rem corrupti similique quasi? Reprehenderit,
+      commodi? Quisquam velit earum blanditiis, est deleniti repudiandae quae
+      iste, aperiam similique expedita id optio enim obcaecati repellat
+      inventore veritatis quaerat nisi, tempora reprehenderit tempore dolorem!
+      Asperiores reprehenderit ducimus itaque corporis debitis quae! Delectus
+      vero sed magnam ut sit numquam reprehenderit voluptatibus. Login Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Eius perspiciatis
+      officiis delectus animi nemo repellendus molestiae sed esse provident
+      aliquid ducimus explicabo adipisci harum, illum unde. Quidem consectetur
+      suscipit libero! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Login Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+      perspiciatis officiis delectus animi nemo repellendus molestiae sed esse
+      provident aliquid ducimus explicabo adipisci harum, illum unde. Quidem
+      consectetur suscipit libero! Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Deserunt ex, rerum fugiat laborum voluptatum quisquam
+      ducimus earum dignissimos nemo nulla eaque nisi provident ad molestiae
+      beatae quas blanditiis nostrum! Odit? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Excepturi iusto perferendis tempore
+      voluptatem quod corporis quidem, iste dolores odit nobis corrupti aut
+      rerum sit laborum nam labore sapiente, accusantium eligendi? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Laboriosam consectetur enim
+      cum ab, pariatur, tempora reiciendis alias expedita saepe, inventore aut
+      perspiciatis. Molestiae, tenetur aliquid corrupti facilis ipsa amet ipsum?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione provident
+      quasi culpa rem neque eos, saepe dolor facere numquam voluptate, esse
+      officiis quis cumque adipisci! Voluptatibus, neque? Eius est culpa quo
+      quos ipsam, recusandae voluptatibus praesentium et alias facilis dolores.
+      Eum dignissimos aut distinctio molestias architecto quisquam eius commodi
+      iusto ipsum, optio debitis rem corrupti similique quasi? Reprehenderit,
+      commodi? Quisquam velit earum blanditiis, est deleniti repudiandae quae
+      iste, aperiam similique expedita id optio enim obcaecati repellat
+      inventore veritatis quaerat nisi, tempora reprehenderit tempore dolorem!
+      Asperiores reprehenderit ducimus itaque corporis debitis quae! Delectus
+      vero sed magnam ut sit numquam reprehenderit voluptatibus. Login Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Eius perspiciatis
+      officiis delectus animi nemo repellendus molestiae sed esse provident
+      aliquid ducimus explicabo adipisci harum, illum unde. Quidem consectetur
+      suscipit libero! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Deserunt ex, rerum fugiat laborum voluptatum quisquam ducimus earum
+      dignissimos nemo nulla eaque nisi provident ad molestiae beatae quas
+      blanditiis nostrum! Odit? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Excepturi iusto perferendis tempore voluptatem quod
+      corporis quidem, iste dolores odit nobis corrupti aut rerum sit laborum
+      nam labore sapiente, accusantium eligendi? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Laboriosam consectetur enim cum ab,
+      pariatur, tempora reiciendis alias expedita saepe, inventore aut
+      perspiciatis. Molestiae, tenetur aliquid corrupti facilis ipsa amet ipsum?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione provident
+      quasi culpa rem neque eos, saepe dolor facere numquam voluptate, esse
+      officiis quis cumque adipisci! Voluptatibus, neque? Eius est culpa quo
+      quos ipsam, recusandae voluptatibus praesentium et alias facilis dolores.
+      Eum dignissimos aut distinctio molestias architecto quisquam eius commodi
+      iusto ipsum, optio debitis rem corrupti similique quasi? Reprehenderit,
+      commodi? Quisquam velit earum blanditiis, est deleniti repudiandae quae
+      iste, aperiam similique expedita id optio enim obcaecati repellat
+      inventore veritatis quaerat nisi, tempora reprehenderit tempore dolorem!
+      Asperiores reprehenderit ducimus itaque corporis debitis quae! Delectus
+      vero sed magnam ut sit numquam reprehenderit voluptatibus. Login Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Eius perspiciatis
+      officiis delectus animi nemo repellendus molestiae sed esse provident
+      aliquid ducimus explicabo adipisci harum, illum unde. Quidem consectetur
+      suscipit libero! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </div>
+  );
+};
+
+export default Login;
