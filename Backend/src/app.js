a308160a5656f4f3aecff0b61eec9dd898eb1c27
iambuzzz <ambujjais1@gmail.com>
2025-12-30 22:47:50 +0530

    done final 2

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -39,10 +39,10 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/", authRouter);
-app.use("/api/", profileRouter);
-app.use("/api/", requestRouter);
-app.use("/api/", userRouter);
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
 
 start();
 
