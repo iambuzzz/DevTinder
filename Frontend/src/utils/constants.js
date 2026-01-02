82863f6e7dbd317dce6f0b7cc35a5c60a23baed1
iambuzzz <ambujjais1@gmail.com>
2026-01-02 19:52:44 +0530

    socket io bug fix

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,2 +1,9 @@
-export const BASE_URL =
-  window.location.hostname === "localhost" ? "http://localhost:3000/" : "/api/";
+const isLocal = window.location.hostname === "localhost";
+
+// Axios/API calls ke liye (Isse baaki sab sahi chalta rahega)
+export const BASE_URL = isLocal ? "http://localhost:3000/" : "/api/";
+
+// Socket connection ke liye (Domain ke bina Socket namespace error deta hai)
+export const SOCKET_URL = isLocal
+  ? "http://localhost:3000/"
+  : "https://iambuzzdev.in/";
--- Frontend/src/utils/socket.js
+++ Frontend/src/utils/socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
-import { BASE_URL } from "./constants";
+import { SOCKET_URL } from "./constants";
 
 const createSocketConnection = () => {
-  return io(BASE_URL, {
+  return io(SOCKET_URL, {
     withCredentials: true, // Cookies bhejne ke liye
     transports: ["websocket"], // Isse 'Bad Request' errors kam ho jati hain
   });
